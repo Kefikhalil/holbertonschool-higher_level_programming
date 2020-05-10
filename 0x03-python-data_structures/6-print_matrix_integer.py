5176b18a0a5e47680b63887d0fa8ab8192e06cd9
Kefikhalil <naroqt@gmail.com>
2020-05-10 19:58:30 +0000

    print matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+   for i in matrix:
+        for e in i:
+            print("{:d}".format(e), end="")
+            if i[-1] != e:
+                print(end=" ")
+        print()
