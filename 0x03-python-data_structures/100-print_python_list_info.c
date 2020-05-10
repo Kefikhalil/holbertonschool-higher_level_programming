dde17df4e5aaadf975ea44ebe6c8a59f7169decd
Kefikhalil <naroqt@gmail.com>
2020-05-10 20:14:07 +0000

    print python list

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,8 +1,3 @@
 #include <Python.h>
 #include <stdio.h>
 void print_python_list_info(PyObject *p)
-for ( ; i < length; i++)
-	{
-		obj = PyList_GetItem(p, i);
-		printf("Element %lu: %s\n", i, Py_TYPE(obj)->tp_name);
-	}
