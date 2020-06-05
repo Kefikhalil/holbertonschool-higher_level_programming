3ca1152733b572988ed5ce39699184142e766846
Kefikhalil <naroqt@gmail.com>
2020-06-05 21:15:28 +0000

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,5 @@
 def read_file(filename=""):
    '''Read file'''
 
-    with open(filename, 'r', encoding="utf-8") as f:
-        for line in f:
-            print(line, end='')
+    with open(filename, 'r', encoding='UTF8') as file:
+        print(file.read(), end="")
