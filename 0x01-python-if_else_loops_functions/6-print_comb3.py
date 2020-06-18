04fe65323194ee1ec63eaa8c45f94640dcebc42b
Kefikhalil <naroqt@gmail.com>
2020-06-18 10:16:54 +0000

    updated

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,6 +5,6 @@ for a in range(0, 10):
     for b in range(a, 10):
         if a != b:
             if a < 8:
-                print("{:d}{:d}".format(a, b), end='\n')
+                print("{:d}{:d}".format(a, b), end=', ')
             else:
                 print("{:d}{:d}".format(a, b), end='\n')
