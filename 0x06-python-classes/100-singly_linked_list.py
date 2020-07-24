1c11cd0bb53d019de627188aee765e6c1aa73208
Kefikhalil <naroqt@gmail.com>
2020-07-23 18:10:52 -0700

    updated

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+class linked list
+"""
+
+
+class Node:
+    """node
+    """
+     def __init__(self, data, next_node=None):
+         """init"""
+        self.data = data
+        self.next_node = None
