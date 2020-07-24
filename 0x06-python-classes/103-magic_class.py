7d2c03acab03c84ec5997b859ce13bc20b7ed461
Kefikhalil <naroqt@gmail.com>
2020-07-23 18:17:50 -0700

    updated

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""magic class
+"""
+
+class MagicClass:
+    """ Magic Class 
+    """
+    def __init__(self, radius=0):
+        """ __init__
+        """
+        self.__radius = 0
