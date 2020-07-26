67bb59abbfb8fca6ef2d0c74592f99c658c06edd
Kefikhalil <naroqt@gmail.com>
2020-07-26 16:25:33 -0700

    updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+
 """
 class Add integers
 """
+
+
+def add_integer(a, b=98):
+    """
+    Add two integers
+    """    
+    if a is None or type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if not type(b) in [int, float]:
+        raise TypeError("b must be an integer")
+    if (a + b) == float('inf') or (a + b) == -float('inf'):
+        return b
+    return int(a) + int(b)
