415b1897c4d1c30f0348591803950949416c8af4
Kefikhalil <naroqt@gmail.com>
2020-07-27 06:58:08 -0700

    updated

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+class base
+"""
+
+
+class Base:
+    """base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """init
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        
