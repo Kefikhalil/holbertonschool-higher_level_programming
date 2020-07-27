3a92c7316b9704ec35429a03f426fb513a630087
Kefikhalil <naroqt@gmail.com>
2020-07-26 17:11:29 -0700

    updated

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+class my int
+"""
+
+
+class MyInt(int):
+    """MyInt
+    """
+    def __eq__(self, other):
+        """eq
+        """
+        return False
+
+    def __ne__(self, other):
+        """ne
+        """
+        return True
