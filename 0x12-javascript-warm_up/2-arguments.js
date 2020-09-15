1584f972146e424dabd98d0453bef12f7c692182
Kefikhalil <naroqt@gmail.com>
2020-09-15 12:54:54 -0700

    updated

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* prints a message depending of the number of arguments passed */
+if (a === 2) {
+  console.log('No argument');
+} else if (a === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
+
