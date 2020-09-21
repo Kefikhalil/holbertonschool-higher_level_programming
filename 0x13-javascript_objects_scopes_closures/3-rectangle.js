fe6e1f2c5815f8b337c0c3bf7155c7913c0010c8
Kefikhalil <naroqt@gmail.com>
2020-09-21 14:28:51 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 /* class Rectangle that defines a rectangle */
-module.exports = Rectangle
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
