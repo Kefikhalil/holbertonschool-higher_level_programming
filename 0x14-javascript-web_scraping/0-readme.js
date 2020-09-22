915f75cd7b6f23ed35f3c30d158547546e3e88d4
Kefikhalil <naroqt@gmail.com>
2020-09-22 14:33:49 -0700

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,9 @@
 /*reads and prints the content of a file*/
 const filename = process.argv[2];
 const fs = require('fs');
-fs.readFile(filename, 'utf-8', function (err, data) {
-  if (err) {
-    console.log(err);
+fs.readFile(filename, 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
   } else {
     console.log(data);
   }
