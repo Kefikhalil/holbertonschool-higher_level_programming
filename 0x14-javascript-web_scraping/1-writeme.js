65e6dc132149c4bbe3e62eb0ce2d2ad519cd6a50
Kefikhalil <naroqt@gmail.com>
2020-09-22 14:30:14 -0700

    updated

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,8 +3,8 @@
 const filename = process.argv[2];
 const fs = require('fs');
 const filecontent = process.argv[3];
-fs.writeFile(filename, filecontent, 'utf-8', function (err) {
-	if (err) {
-		console.log(err);
+fs.writeFile(filename, filecontent, 'utf-8', function (error) {
+	if (error) {
+		console.log(error);
 	}
 });
